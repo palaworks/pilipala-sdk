d0711d256e839a7777fbe5de24742be43d9060d6
Thaumy <thaumy@outlook.com>
2022-08-26 20:20:56 +0800

    Squashed commit of the following:
    commit c1b81e966356c7adbe0af070791493ee66e6652c
    Author: Thaumy <thaumy@outlook.com>
    Date:   Fri Aug 26 20:20:47 2022 +0800
    
        Update html.fs
    
    commit d10c66bf9ede71713274c4ab9d430b997155be20
    Author: Thaumy <thaumy@outlook.com>
    Date:   Wed Aug 17 08:32:13 2022 +0800
    
        add Iterator to entity data
    
    commit 68eeaf0b41931c700bac9fe863f37747964e6ceb
    Author: Thaumy <thaumy@outlook.com>
    Date:   Thu Aug 11 23:36:41 2022 +0800
    
        change id from u64 to i64
    
    commit c64927e6def1ed31981a2f02d114de2dc02d7477
    Author: Thaumy <thaumy@outlook.com>
    Date:   Wed Aug 10 14:00:53 2022 +0800
    
        add log helper
    
    commit 58d82549c9431d67a2a8698983c62da5f665952f
    Author: Thaumy <thaumy@outlook.com>
    Date:   Sat Aug 6 21:42:12 2022 +0800
    
        util.di
    
    commit f9843c79631f2b1fc8c5bc1115969953c355003d
    Author: Thaumy <thaumy@outlook.com>
    Date:   Sat Aug 6 09:29:19 2022 +0800
    
        update dependency
    
    commit 32fea89d98c0ff51ca3da1deced6811f4e93344f
    Author: Thaumy <thaumy@outlook.com>
    Date:   Sat Aug 6 02:59:04 2022 +0800
    
        wrapped types, getFstRow
    
    commit f125f811b877fabcdb9df32acbbc20a098ea9285
    Author: Thaumy <thaumy@outlook.com>
    Date:   Fri Aug 5 18:21:02 2022 +0800
    
        pipeline interfaces
    
    commit ae70714399b53361f079ec5ab4b9e971e3b0d621
    Author: Thaumy <thaumy@outlook.com>
    Date:   Fri Aug 5 11:27:15 2022 +0800
    
        update readme.md
    
    commit f0029f6fee947136e9b634a5b4f58c3eec069691
    Author: Thaumy <thaumy@outlook.com>
    Date:   Thu Aug 4 20:54:08 2022 +0800
    
        update pipelines
    
    commit 97424f2e581100515ff4b0f7eec8128d7fcf267b
    Author: Thaumy <thaumy@outlook.com>
    Date:   Wed Aug 3 10:55:28 2022 +0800
    
        updated pipelines

--- pilipala.util/src/text/html.fs
+++ pilipala.util/src/text/html.fs
@@ -6,6 +6,9 @@ open System.Text.RegularExpressions
 
 type Html = { html: string }
 
+let inline private rm p i = Regex.Replace(i, p, "")
+let inline private replace p i (r: string) = Regex.Replace(i, p, r)
+
 type Html with
     /// 去除html标签
     member self.withoutTags() =
@@ -13,13 +16,27 @@ type Html with
         | null
         | "" -> ""
         | _ ->
-            let inline rm p i = Regex.Replace(i, p, "")
 
             self.html
             |> rm "<script[^>]*>(.|\n)*?</script>" //脚本标签
             |> rm "<style>(.|\n)*</style>" //样式标签
             |> rm "<([^>]|\n)+>" //其他标签
             |> rm "^\s*\n" //空行去除（不考虑最后一行，后续逻辑会将其去除）
-            |> fun s -> Regex.Replace(s, "&#*\w+;", " ") //将转义替换为空格
-            |> fun s -> Regex.Replace(s, "[\s\n]+", " ") //多空白合并
+            |> replace "&#*\w+;" " " //将转义替换为空格
+            |> replace "[\s\n]+" " " //多空白合并
             |> rm "^ | $" //首尾去空格
+
+    /// 字数估计
+    member self.wordCountEstimate() =
+        match self.html with
+        | null
+        | "" -> 0
+        | _ ->
+
+            self.html
+            |> rm "<script[^>]*>(.|\n)*?</script>" //脚本标签
+            |> rm "<style>(.|\n)*</style>" //样式标签
+            |> rm "<([^>]|\n)+>" //其他标签
+            |> rm "[，,。.《》（(）)！!“”‘’？?/\s\n]" //空白和常用标点去除
+            |> rm "&#*\w+;" //去除转义
+            |> fun s -> s.Length
